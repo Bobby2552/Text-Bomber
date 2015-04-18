93d5b2fe65c5cb590d3eac20e157b82ffeadb145
Ben Scholer <benscholer3248511@gmail.com>
2015-04-17 22:42:05 -0400

    Have almost achived basic functionality.

--- app/src/main/java/com/benscholer/textbomber/Bomb.java
+++ app/src/main/java/com/benscholer/textbomber/Bomb.java
@@ -1,6 +1,8 @@
 package com.benscholer.textbomber;
 
 import android.content.Context;
+import android.telephony.SmsManager;
+import android.widget.Toast;
 
 /**
  * Created by benscholer on 4/17/15.
@@ -10,15 +12,21 @@ public class Bomb {
 	public String message;
 	public int repeat;
 
-	public Bomb () {}
+	public Bomb() {
+	}
 
-	public Bomb (String number, String message, int repeat) {
+	public Bomb(String number, String message, int repeat) {
 		this.number = number;
 		this.message = message;
 		this.repeat = repeat;
 	}
 
 	public void send(Context context) {
-		
+		for (int i = 0; i < repeat; i++) {
+			SmsManager sms = SmsManager.getDefault();
+			sms.sendTextMessage(number, null, message, null, null);
+		}
+
+		Toast.makeText(context, "Sending complete.", Toast.LENGTH_SHORT).show();
 	}
 }
--- app/src/main/java/com/benscholer/textbomber/MainActivity.java
+++ app/src/main/java/com/benscholer/textbomber/MainActivity.java
@@ -5,18 +5,47 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
 	Toolbar toolbar;
+	EditText phoneNumber;
+	EditText message;
+	EditText repeat;
+	Button send;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		phoneNumber = (EditText) findViewById(R.id.phone_number);
+		message = (EditText) findViewById(R.id.message);
+		repeat = (EditText) findViewById(R.id.repeat);
+		send = (Button) findViewById(R.id.send_button);
 		toolbar = (Toolbar) findViewById(R.id.toolbar);
 		toolbar.setTitle("Text Bomber");
+
+		send.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String num = phoneNumber.getText().toString();
+				String mssg = message.getText().toString();
+				int rept = Integer.parseInt(repeat.getText().toString());
+
+				if ((num == null || num.equals("")) || (mssg == null || mssg.equals(""))) {
+					Toast.makeText(getApplicationContext(), "Make sure all fields are filled",
+							Toast.LENGTH_LONG).show();
+				} else {
+					Bomb bomb = new Bomb(num, mssg, rept);
+					bomb.send(getApplicationContext());
+				}
+			}
+		});
 	}
 }
